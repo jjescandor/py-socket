4a7074f95dea2e0a4d26edb9b2dd79b127e7416a
JJ Escandor <jj.p.escandor@gmail.com>
2022-10-01 19:05:12 -0400

    worked on client app

--- server.py
+++ server.py
@@ -5,21 +5,32 @@ HEADER = 64
 PORT = 5050
 # SERVER = socket.gethostbyname(socket.gethostname())
 SERVER = "192.168.0.220"
+FORMAT = "utf-8"
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ADDR = (SERVER, PORT)
 server.bind(ADDR)
-
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected. ")
 
     connected = True
     while connected:
-        msg = conn.recv()
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr} {msg}]")
+
+    conn.close()
 
 def start():
     server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         print("Listening for connection...")
         conn, addr = server.accept()
